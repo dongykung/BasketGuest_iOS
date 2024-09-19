f1fec1fc6ff7649dcd48f6954c645d4137d0e75c
kim dong kyung <kdkd422@naver.com>
2024-09-19 15:15:07 +0900

    Add 소셜 미디어 버튼

--- BasketGuest/BasketGuest/Presentation/View/Component/SocialLoginButton.swift
+++ BasketGuest/BasketGuest/Presentation/View/Component/SocialLoginButton.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct SocialLoginButton: View {
+    
+    private var loginText: String
+    private var loginLogo: Image
+    private var maxWidth: CGFloat
+    private var maxHeight: CGFloat
+    private var textColor: Color
+    private var backgroundColor: Color
+    private var loginAction: () -> Void
+    
+    init(loginText: String,
+         loginLogo: Image,
+         maxWidth: CGFloat,
+         maxHeight: CGFloat,
+         textColor: Color,
+         backgroundColor: Color,
+         loginAction: @escaping () -> Void) {
+        self.loginText = loginText
+        self.loginLogo = loginLogo
+        self.maxWidth = maxWidth
+        self.maxHeight = maxHeight
+        self.textColor = textColor
+        self.backgroundColor = backgroundColor
+        self.loginAction = loginAction
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            loginAction()
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(backgroundColor)
+                    .frame(maxWidth: maxWidth,
+                           maxHeight:maxHeight)
+                
+                // 텍스트는 중앙에 배치
+                Text(loginText)
+                    .foregroundStyle(textColor)
+                    .font(.title2)
+                    .frame(maxWidth: maxWidth, maxHeight: maxHeight, alignment: .center)
+                    .padding()
+                HStack {
+                    loginLogo
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: maxHeight * 0.7)
+                        .padding(.horizontal, 12)
+                    
+                    Spacer()
+                }
+                .padding()
+            }
+            .frame(maxWidth: maxWidth,
+                   maxHeight:maxHeight)
+        }
     }
 }
 
 #Preview {
-    SocialLoginButton()
+    //SocialLoginButton()
 }
