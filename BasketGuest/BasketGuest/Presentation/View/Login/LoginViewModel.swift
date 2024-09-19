a9937dfb2cb689fed4cb324f5a5ac18b408c3bc2
kim dong kyung <kdkd422@naver.com>
2024-09-19 15:16:10 +0900

    Add 로그인 뷰모델

--- BasketGuest/BasketGuest/Presentation/View/Login/LoginViewModel.swift
+++ BasketGuest/BasketGuest/Presentation/View/Login/LoginViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+
+class LoginViewModel: ObservableObject {
+    
+    enum Action {
+        case google
+        case kakao
+    }
+    
+    
+    
+    func send(_ action: Action) {
+        switch action {
+        case .google:
+            return
+        case .kakao:
+            return
+        }
+    }
+    
+}
