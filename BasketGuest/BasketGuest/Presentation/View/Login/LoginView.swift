e57efa957373c9ca91390fd690a3abf2df45e606
kim dong kyung <kdkd422@naver.com>
2024-09-19 15:15:50 +0900

    Add 로그인 뷰

--- BasketGuest/BasketGuest/Presentation/View/Login/LoginView.swift
+++ BasketGuest/BasketGuest/Presentation/View/Login/LoginView.swift
@@ -9,7 +9,54 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            ZStack {
+                //백그라운드 배경 뷰
+                BackgroundView()
+                
+                VStack(spacing: 12) {
+                    //빈 공간
+                    Spacer()
+                        .frame(maxHeight: proxy.size.height * 0.65)
+                        .background(.orange)
+                    
+                    
+                    SocialLoginButton(
+                        loginText: "구글 로그인",
+                        loginLogo: Image(.google),
+                        maxWidth: proxy.size.width * 0.9,
+                        maxHeight: proxy.size.height * 0.07,
+                        textColor: .black,
+                        backgroundColor: .white) {
+                            
+                        }
+                    
+                    SocialLoginButton(
+                        loginText: "카카오 로그인",
+                        loginLogo: Image(.kakao),
+                        maxWidth: proxy.size.width * 0.9,
+                        maxHeight: proxy.size.height * 0.07,
+                        textColor: .black,
+                        backgroundColor: .kakaocolor) {
+                            
+                        }
+                    
+                    SocialLoginButton(
+                        loginText: "애플 로그인",
+                        loginLogo: Image(.apple),
+                        maxWidth: proxy.size.width * 0.9,
+                        maxHeight: proxy.size.height * 0.07,
+                        textColor: .white,
+                        backgroundColor: .black) {
+                            
+                        }
+                }
+                .frame(width: proxy.size.width, height: proxy.size.height)
+                
+                
+                
+            }
+        }
     }
 }
 
